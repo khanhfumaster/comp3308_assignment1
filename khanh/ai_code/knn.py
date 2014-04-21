5b504e89e5b1b644717312f5d0e5f7a456d29855
Khanh Nguyen <khanh@intersect.org.au>
2014-04-21 17:16:18 +1000

    Change knn object to allow multiple tests in prep for 10 folds

--- khanh/ai_code/knn.py
+++ khanh/ai_code/knn.py
@@ -2,32 +2,28 @@ from math import sqrt
 from operator import itemgetter
 
 class KNearestNeighbours():
-	def __init__(self, patient_list, test_patient, k):
-		self.patient_list = patient_list
-		self.test_patient = test_patient
-		self.k = k
-		# run the calculate method
-		self._calculate()
+	def __init__(self, training_data):
+		self.training_data = training_data
 		
-	def _calculate(self):
+	def on(self, test_patient, k):
 		# Eucledian distance
 		# D(A,B) = sqrt((a1-b1)^2 + (a2-b2)^2 + ... + (an-bn)^2)
 
-		for patient in self.patient_list:
+		for patient in self.training_data:
 			inner_sum = 0
 			for attr in patient:
 				if attr != 'class':
-					inner_sum += (self.test_patient[attr] - patient[attr])**2
+					inner_sum += (test_patient[attr] - patient[attr])**2
 			patient['distance'] = sqrt(inner_sum)
 
 		# sort the list by distance calculated
-		sorted_list = sorted(self.patient_list, key=itemgetter('distance')) 
+		sorted_list = sorted(self.training_data, key=itemgetter('distance')) 
 
 		# only take the nearest k
 		class0_counter = 0
 		class1_counter = 0
 
-		for neighbour in sorted_list[:self.k]:
+		for neighbour in sorted_list[:k]:
 			if neighbour['class'] == "class0":
 				class0_counter += 1
 			elif neighbour['class'] == "class1":
@@ -35,8 +31,8 @@ class KNearestNeighbours():
 
 		if class0_counter == class1_counter:
 			#TODO: decide on a tie breaker method
-			print "Tie. Deal with it."
+			return "Tie. Deal with it."
 		elif class0_counter > class1_counter:
-			print "class0"
+			return "class0"
 		elif class1_counter > class0_counter:
-			print "class1"
+			return "class1"
