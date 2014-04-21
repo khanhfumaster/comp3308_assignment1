29d56f0d4fd821c32033e8e5721dc342765fef29
= <ajungy@gmail.com>
2014-04-22 01:15:00 +1000

    first upload

---
+++ jung/knn.py
@@ -0,0 +1,37 @@
+import copy
+from math import sqrt
+from operator import itemgetter
+from random import randint
+
+class kNN:
+    def __init__(self, training_data):
+        self._training_data = copy.deepcopy(training_data)
+        
+    def classify(self, new_instance, k):
+        for instance in self._training_data:
+            distance = 0.0
+            for attr in instance:
+                if attr != 'class' and attr != 'distance':
+                    distance += (instance[attr] - new_instance[attr])**2
+            instance['distance'] = sqrt(distance)
+
+        sorted_list = sorted(self._training_data, key=itemgetter('distance'))
+        class0_count = 0
+        class1_count = 1
+
+        for neighbour in sorted_list[:k]:
+            if neighbour['class'] == 'class0':
+                class0_count += 1
+            if neighbour['class'] == 'class1':
+                class1_count += 1
+
+        if class0_count == class1_count:
+            rand = randint(0, 1)
+            if rand == 0:
+                return 'class0'
+            if rand == 1:
+                return 'class1'
+        elif class0_count > class1_count:
+            return 'class0'
+        elif class1_count > class0_count:
+            return 'class1'
---
+++ jung/naive_bayes.py
@@ -0,0 +1,47 @@
+import copy
+import numpy
+from math import sqrt, exp, pi
+from random import randint
+
+class naiveBayes():
+    def __init__(self, training_data):
+        self._training_data = copy.deepcopy(training_data)
+
+    # P(class)
+    def p_class(self, classifier):
+        count = 0.0
+        total = float(len(self._training_data))
+        for instance in self._training_data:
+            if instance['class'] == classifier:
+                count += 1
+        return count/total
+
+    # P(E = x|class)
+    def p_e_class(self, key, val, classifier):
+        keys = []
+        for instance in self._training_data:
+            if instance['class'] == classifier:
+                keys.append(instance[key])
+        mean = numpy.mean(keys)
+        std = numpy.std(keys)
+        return 1/((std * sqrt(2*pi)) * exp(-((val-mean)**2)/(2*(std**2))))
+    
+    def classify(self, new_instance):
+        results = {}
+        for classifier in ['class0', 'class1']:
+            p = self.p_class(classifier)
+            for attr in new_instance:
+                p *= self.p_e_class(attr, new_instance[attr], classifier)
+            results[classifier] = p
+
+        if results['class0'] == results['class1']:
+            rand = randint(0, 1)
+            if rand == 0:
+                return 'class0'
+            if rand == 1:
+                return 'class1'
+        elif results['class0'] > results['class1']:
+            return 'class0'
+        elif results['class0'] < results['class1']:
+            return 'class1'
+            
